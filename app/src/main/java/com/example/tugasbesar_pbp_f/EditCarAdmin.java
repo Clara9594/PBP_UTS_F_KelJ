65c47fbb64def2991dbec7d8bd79b0a262af6076
Clara9594 <180709594@students.uajy.ac.id>
2020-11-09 23:00:48 +0700

    1

---
+++ app/src/main/java/com/example/tugasbesar_pbp_f/EditCarAdmin.java
@@ -0,0 +1,14 @@
+package com.example.tugasbesar_pbp_f;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditCarAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_car_admin);
+    }
+}
